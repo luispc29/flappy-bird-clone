c98fec44339d754532e1fef0e6ee27865c4a1064
Luis Castillo <90541323+luispc29@users.noreply.github.com>
2023-09-11 20:37:30 -0700

    created most files and added images

---
+++ flappybird.js
@@ -0,0 +1,12 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+window.onload = function(){
+    board = document.getElementById("board");
+    board.height =boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); //used for drawing on the board
+}
